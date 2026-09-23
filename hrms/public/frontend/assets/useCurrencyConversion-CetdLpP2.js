fc263ff5e3a23e96f4507dbdc61e1403d02cd61f
Realtyna HRMS <hr@realtyna.com>
2026-09-23 21:03:09 +0400

    chore: rebuild frontend assets for timer project name

--- hrms/public/frontend/assets/useCurrencyConversion-CetdLpP2.js
+++
@@ -1,2 +0,0 @@
-import{w as c}from"./frappe-ui--3g6q0lc.js";function i(n,t,l=[]){const u=new Set([...l]),r=()=>{var e;(e=n.data)==null||e.forEach(a=>{a!=null&&a.fieldname&&u.has(a.fieldname)&&(!a._original_label&&a.label&&(a._original_label=a.label.replace(/\([^\)]*\)/g,"").trim()),u.has(a.fieldname)&&(a.label=`${a._original_label} (${t.value.currency})`))})};return c(()=>{var e;return(e=t.value)==null?void 0:e.currency},()=>{r()},{immediate:!0}),c(()=>n.data,()=>{r()},{deep:!0,immediate:!0}),{updateLabels:r}}export{i as u};
-//# sourceMappingURL=useCurrencyConversion-CetdLpP2.js.map
